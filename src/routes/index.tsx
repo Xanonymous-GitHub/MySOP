ceeaedd1fa17444925ebf7de82f0ffe1a6ecf380
Xanonymous <trusaidlin@gmail.com>
2022-11-18 16:24:55 +0800

    feat: add ios aos location change

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,19 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import "./main.css";
+import uaParser from "ua-parser-js";
 
 export default component$(() => {
+  const os = uaParser().os.name;
+  const pdfPath = "pdf/SOP.pdf";
+
+  if (os === "iOS" || os === "Android") {
+    location.replace(pdfPath);
+  }
+
   return (
     <>
-      <embed type="application/pdf" class="pdf-container" src="/pdf/SOP.pdf" />
+      <embed type="application/pdf" class="pdf-container" src={pdfPath} />
     </>
   );
 });
