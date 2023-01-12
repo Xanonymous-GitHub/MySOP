1ea4a96a08376b1cb8b31353af33547c1b8ca501
Xanonymous <trusaidlin@gmail.com>
2023-01-13 03:55:52 +0800

    refactor: remove unneeded ua parser

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,9 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import "./main.css";
-import uaParser from "ua-parser-js";
 
 export default component$(() => {
-  const os = uaParser().os.name;
   const pdfPath = "pdf/SOP.pdf";
-
-  if (os === "iOS" || os === "Android") {
-    location.replace(pdfPath);
-  }
 
   return (
     <>
@@ -19,5 +13,5 @@ export default component$(() => {
 });
 
 export const head: DocumentHead = {
-  title: "SOP TU Lin",
+  title: "TU Lin's Documents",
 };
